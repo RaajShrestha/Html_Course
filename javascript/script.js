24daed3d64dc420e9eb2ab5f9ee924114b9c58ce
Rajj <raajshrestha1999@gmail.com>
2023-07-09 08:17:50 +0545

    index

---
+++ javascript/script.js
@@ -0,0 +1,50 @@
+// let a ="harry";
+// let b =5;
+// console.log(a+b);
+
+// const dict={
+//     apple: "Its is a fruit name",
+//     orange: "Its is a fruit name",
+//     Mango: "Its is a fruit name"
+// }
+// console.log(dict["Mango"])
+
+// Chapter-2 //
+// let age=prompt("What is your age?");
+// if (age>10 && age<20){
+//     console.log("You age lies between 10 and 20");
+// }
+// else{
+//     console.log("Your age doesn't is between 10 and 20");
+// }
+
+// problem 2
+// let age=prompt("What is your age?")
+// switch(age){
+//     case 12:
+//         console.log("Your age is 12")
+//     case 13:
+//         console.log("Your age is 13")
+//     case 14:
+//         console.log("Your age is 14")
+//     case 15:
+//         console.log("Your age is 15")
+//     default:
+//         console.log("Your age is not special")
+// }
+//  problem 3
+// let num=prompt("What is your age!")
+// num=Number.parseInt(num)
+// if(num%2==0 && num%3==0){
+//     console.log("Your number is divisible by 2 and 3")
+// }
+// else{
+//     console.log("Yopur number is not dividible by 2 and 3")
+// }
+// // let x = myFunction(4, 3);
+// document.getElementById("demo").innerHTML = x;
+
+// function myFunction(a, b) {
+//   return a * b;
+// }
+
---
+++ javascript/tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+// let a ="harry";
+// let b =5;
+// console.log(a+b);
+
+// const dict={
+//     apple: "Its is a fruit name",
+//     orange: "Its is a fruit name",
+//     Mango: "Its is a fruit name"
+// }
+// console.log(dict["Mango"])
+let x = myFunction(4, 3);
+document.getElementById("demo").innerHTML = x;
+
+function myFunction(a, b) {
+  return a * b;
+}
